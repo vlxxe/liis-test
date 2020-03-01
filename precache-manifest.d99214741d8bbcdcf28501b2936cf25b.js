c563f715efdffcf9360f9217afc2ebe696b7d82f
vlxxe <tokzic01@gmail.com>
2020-03-01 13:15:35 +0300

    Updates

---
+++ precache-manifest.d99214741d8bbcdcf28501b2936cf25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2c022072fcf2e13e92dfce8a627789",
+    "url": "/liis-test/index.html"
+  },
+  {
+    "revision": "c81c1d367f156e8ab83c",
+    "url": "/liis-test/static/css/main.8e7d43f9.chunk.css"
+  },
+  {
+    "revision": "ba6c32b1f94fe0dd731e",
+    "url": "/liis-test/static/js/2.ef2aac77.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/liis-test/static/js/2.ef2aac77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81c1d367f156e8ab83c",
+    "url": "/liis-test/static/js/main.fcbd92c4.chunk.js"
+  },
+  {
+    "revision": "27a532f80162a3992496",
+    "url": "/liis-test/static/js/runtime-main.0de400cf.js"
+  }
+]);
